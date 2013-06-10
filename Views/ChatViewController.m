14786a76abaf5b348375fecabed9bea547ff9869
Soroush Pour <me@soroushjp.com>
2013-06-10 15:40:44 -0700

    Reorganized file structure, everything staged this time

---
+++ Views/ChatViewController.m
@@ -0,0 +1,38 @@
+//
+//  ChatController.m
+//  LiveMobile
+//
+//  Created by Soroush Pour on 5/06/13.
+//  Copyright (c) 2013 Soroush Pour. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@end
+
+@implementation ChatViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
